4231592602b3a371c20af3e79a095b95d660184b
LeeMinKaHo <leminhkhoa5241@gmail.com>
2025-01-12 21:19:25 +0700

    make wishlist , add best Sellers in Home-Page

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice'; // Đảm bảo import đúng cartReducer
-import authReducer from './features/auth/authSlice'
+import authReducer from './features/auth/authSlice';
+import wishlistReducer from "./features/wishlist/wishList"
 export const store = configureStore({
   reducer: {
     cart: cartReducer, // Sử dụng cartReducer trong slice đã tạo
-    auth: authReducer
+    auth: authReducer,
+    wishlist: wishlistReducer
   },
 });
